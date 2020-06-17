437e34c04c1e61902142ea3d16914950ba09ef88
UFo_x <tero.jaakkola1@gmail.com>
2020-06-17 15:55:31 +0300

    feat(photo): create single photo page

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -2,6 +2,11 @@ import React from "react";
 import fetch from "../lib/fetch";
 import { useRouter } from "next/router";
 import useSWR from "swr";
+import Layout from "../components/layout";
+import Header from "../components/header";
+import Footer from "../components/footer";
+import Content from "../components/content";
+import Link from "next/link";
 
 const Item = () => {
   const router = useRouter();
@@ -9,7 +14,37 @@ const Item = () => {
   const { data: photo } = useSWR("/api/photo?id=" + (id || 0), fetch);
 
   if (!photo) return <div>loading</div>;
-  return <p>Post: {photo.title}</p>;
+  return (
+    <>
+      <Layout>
+        <Header />
+        <Content>
+          <Link href="/">
+            <a className="uppercase text-xl font-black text-gray-600 hover:text-gray-800">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-8 h-8 fill-current inline"
+                viewBox="0 0 24 24"
+              >
+                <path d="M14.7 15.3a1 1 0 01-1.4 1.4l-4-4a1 1 0 010-1.4l4-4a1 1 0 011.4 1.4L11.42 12l3.3 3.3z" />
+              </svg>
+              back
+            </a>
+          </Link>
+          <h2 className="text-4xl text-gray-800 uppercase">{photo.title}</h2>
+          <hr className="my-4" />
+          <img
+            className="shadow-lg w-600 h-600"
+            key={photo.id}
+            src={photo.url}
+            loading="lazy"
+            alt={photo.title}
+          />
+        </Content>
+        <Footer />
+      </Layout>
+    </>
+  );
 };
 
 export default Item;
